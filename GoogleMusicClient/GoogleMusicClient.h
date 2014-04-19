6fbf8c7bd4f1e092c1fd9a7b769bcb23f40e67b5
Elliot Chance <elliotchance@gmail.com>
2014-04-19 17:51:18 +1000

    Initial Commit

---
+++ GoogleMusicClient/GoogleMusicClient.h
@@ -0,0 +1,13 @@
+//
+//  GoogleMusicClient.h
+//  GoogleMusicClient
+//
+//  Created by Elliot Chance on 19/04/2014.
+//  Copyright (c) 2014 Elliot Chance. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GoogleMusicClient : NSObject
+
+@end
---
+++ GoogleMusicClient/GoogleMusicClient.m
@@ -0,0 +1,13 @@
+//
+//  GoogleMusicClient.m
+//  GoogleMusicClient
+//
+//  Created by Elliot Chance on 19/04/2014.
+//  Copyright (c) 2014 Elliot Chance. All rights reserved.
+//
+
+#import "GoogleMusicClient.h"
+
+@implementation GoogleMusicClient
+
+@end
